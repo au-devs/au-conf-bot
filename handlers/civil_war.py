08328a9d51efc669d4869cabc559b066e9fa9116
Kirill Tokarev <tokarevki2997@gmail.com>
2026-04-21 01:43:54 +0500

    Fix typo in success image filename for civil war feature, changing from "civilvar.jpg" to "civilwar.jpg".

--- handlers/civil_war.py
+++ handlers/civil_war.py
@@ -27,7 +27,7 @@ def get_assets_dir() -> Path:
 
 
 def get_success_image_path() -> Path:
-    return get_assets_dir() / "civilvar.jpg"
+    return get_assets_dir() / "civilwar.jpg"
 
 
 def get_fail_image_path() -> Path:
